061c2b12e72192cb36cb32f14a20f45c4a0610f5
Vishesh <visheshsingh16@gmail.com>
2018-05-31 13:04:27 -0500

    extracting css to a file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   module: {
@@ -18,6 +19,10 @@ module.exports = {
             options: { minimize: true }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
   },
@@ -25,6 +30,10 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
   ]
 };
