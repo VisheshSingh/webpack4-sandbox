4462a891d9ec73383c31e4966ceb3d417a5df0e2
Vishesh <visheshsingh16@gmail.com>
2018-05-31 12:57:55 -0500

    the Html Webpack plugin and html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -7,7 +9,22 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
